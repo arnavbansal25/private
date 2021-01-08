c216a1308b54ae51f6c2ef450e2c4229f89847a3
Arnav Bansal <arnavbansal.8232@gmail.com>
2021-01-08 13:43:16 +0530

    added functionality (encryption)

--- Js/index.js
+++ Js/index.js
@@ -18,3 +18,35 @@ function upload() {
   document.getElementById("p2").style.visibility = "visible";
   document.getElementById("p3").style.visibility = "visible";
 }
+
+
+
+
+
+function encrypt() {
+   
+  encr_img = new SimpleImage(orig_img.getWidth(), orig_img.getHeight());
+
+  for(var pi of orig_img.values()){
+    encr_img.setPixel(pi.getX(),pi.getY(),pi);
+  }
+
+  for(var pi of encr_img.values()){
+    var r1 = pi.getRed();
+    var g1 = pi.getGreen();
+    var b1 = pi.getBlue();
+
+    r1 = (r1+key)%256;
+    g1 = (g1+key)%256;
+    b1 = (b1+key)%256;
+
+    pi.setRed(r1);
+    pi.setGreen(g1);
+    pi.setBlue(b1);
+  }
+
+  encr_img.drawTo(can2);
+  document.getElementById("p2").style.visibility = "hidden";
+
+  document.getElementById("download_btn").setAttribute('download', "encrypted");
+}
